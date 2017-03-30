34713071c9bfa5bf4a352f6bed3363eb20d412b6
Pito Salas <pitosalas@gmail.com>
2017-03-30 17:57:03 -0400

    small

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,5 +1,6 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
+    console.log("recevived pito");
     document.querySelector("textarea.form-control").value=this.renderMessage(data);
   },
 
